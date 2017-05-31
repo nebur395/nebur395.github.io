5bd3d3414f907b6b6151fb7cb752c21b512e9360
Ruben Moreno Jimeno <nebur395@hotmail.com>
2017-05-31 18:48:00 +0200

    Modified projects card style

--- public/javascript/controllers/projectsController.js
+++ public/javascript/controllers/projectsController.js
@@ -6,6 +6,7 @@ angular.module('rubenMorenoApp')
             {
                 title: "Añisclo's POI",
                 link: "https://github.com/nebur395/Anisclo",
+                webPage: "",
                 secondaryTitle: "Web Application - Responsive",
                 image: "poisAnisclo.png",
                 description: "Proyecto para la universidad que consiste en una red social para" +
@@ -15,7 +16,8 @@ angular.module('rubenMorenoApp')
             {
                 title: "Smart CampUZ",
                 link: "https://github.com/UNIZAR-30249-2016-SmartCampUZ/smartCampUZ",
-                secondaryTitle: "Web Application - Responsive (But not optimized)",
+                webPage: "",
+                secondaryTitle: "Web Application - Responsive",
                 image: "smartCampUZ.png",
                 description: "Proyecto para la universidad que consiste en una aproximación de" +
                 " una Smart City para el Campus de Unizar."
@@ -24,7 +26,8 @@ angular.module('rubenMorenoApp')
             {
                 title: "No te rayes CO",
                 link: "https://github.com/nebur395/noTeRayesCo",
-                secondaryTitle: "https://noterayesco.herokuapp.com/",
+                webPage: "https://noterayesco.herokuapp.com/",
+                secondaryTitle: "Web Application - Responsive",
                 image: "noTeRayesCo.png",
                 description: "Página web con una cuenta atrás para las fiestas del pilar de" +
                 " Zaragoza, España."
@@ -33,6 +36,7 @@ angular.module('rubenMorenoApp')
             {
                 title: "REC & MAKE",
                 link: "https://github.com/nebur395/uCode17",
+                webPage: "",
                 secondaryTitle: "Web Application - Responsive",
                 image: "ruben.jpg",
                 description: "Proyecto para el hackathon uCode by Adidas 2017."
@@ -41,7 +45,8 @@ angular.module('rubenMorenoApp')
             {
                 title: "Onirian Official Website",
                 link: "https://github.com/nebur395/Onirian_2.0",
-                secondaryTitle: "https://onirian.herokuapp.com/",
+                webPage: "https://onirian.herokuapp.com/",
+                secondaryTitle: "Web Application - Responsive",
                 image: "onirian.png",
                 description: "Página web oficial del grupo de música Onirian de Zaragoza, España."
             },
@@ -49,6 +54,7 @@ angular.module('rubenMorenoApp')
             {
                 title: "Training Tracker",
                 link: "https://github.com/UNIZAR-30248-2016-TrainingTracker/trainingTracker",
+                webPage: "",
                 secondaryTitle: "Web Application - Responsive",
                 image: "trainingTracker.png",
                 description: "Proyecto para la universidad que consiste en una aplicación para" +
@@ -58,6 +64,7 @@ angular.module('rubenMorenoApp')
             {
                 title: "AirQ",
                 link: "https://github.com/wynro/airQ-web",
+                webPage: "",
                 secondaryTitle: "Web Application - Responsive",
                 image: "ruben.jpg",
                 description: "Proyecto para el hackathon International Space Apps Challenge 2016 de la NASA."
