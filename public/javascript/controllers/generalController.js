b881d2234db64525339d0cb0c67e917fa0b5c5e8
Ruben Moreno Jimeno <nebur395@hotmail.com>
2017-06-01 01:49:21 +0200

    Added toTopButton

--- public/javascript/controllers/generalController.js
+++ public/javascript/controllers/generalController.js
@@ -2,6 +2,9 @@ angular.module('rubenMorenoApp')
 
     .controller('generalCtrl', ['$scope', function ($scope) {
 
+        $scope.appearClass = false;
+        $scope.disappearClass = false;
+
         // Add smooth scrolling on [hash] links
         $scope.smoothScrolling = function (link, clickEvent) {
 
@@ -22,5 +25,23 @@ angular.module('rubenMorenoApp')
                     window.location.hash = hash;
                 });
             } // End if
+        };
+
+        window.onscroll = function (e) {
+            if(document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+                $scope.appearClass = true;
+                $scope.disappearClass = false;
+                $scope.$apply();
+                /*$("#toTopButton").addClass("appear");
+                $("#toTopButton").removeClass("disappear");*/
+                //document.getElementById("toTopButton").style.display = "block";
+            } else if ($scope.appearClass) {
+                $scope.appearClass = false;
+                $scope.disappearClass = true;
+                $scope.$apply();
+                /*$("#toTopButton").addClass("disappear");
+                $("#toTopButton").removeClass("appear");*/
+                //document.getElementById("toTopButton").style.display = "none";
+            }
         }
     }]);
