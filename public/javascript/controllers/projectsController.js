de34ebcc7e478a7ac099820c1817b5144369e098
Ruben Moreno Jimeno <nebur395@hotmail.com>
2017-05-31 14:25:21 +0200

    Added project cards style

--- public/javascript/controllers/projectsController.js
+++ public/javascript/controllers/projectsController.js
@@ -2,4 +2,46 @@ angular.module('rubenMorenoApp')
 
     .controller('projectsCtrl', ['$scope', function ($scope) {
 
+        $scope.projectList = [
+            {
+                title: "Training Tracker",
+                link: "https://github.com/wynro/airQ-web",
+                secondaryTitle: "Web Application",
+                image: "ruben.jpg",
+                description: "Proyecto para el International Space Apps Challenge 2016 de la NASA."
+            },
+
+            {
+                title: "Training Tracker",
+                link: "https://github.com/wynro/airQ-web",
+                secondaryTitle: "Web Application",
+                image: "ruben.jpg",
+                description: "Proyecto para el International Space Apps Challenge 2016 de la NASA."
+            },
+
+            {
+                title: "Training Tracker",
+                link: "https://github.com/wynro/airQ-web",
+                secondaryTitle: "Web Application",
+                image: "ruben.jpg",
+                description: "Proyecto para el International Space Apps Challenge 2016 de la NASA."
+            },
+
+            {
+                title: "Training Tracker",
+                link: "https://github.com/wynro/airQ-web",
+                secondaryTitle: "Web Application",
+                image: "ruben.jpg",
+                description: "Proyecto para el International Space Apps Challenge 2016 de la NASA."
+            },
+
+            {
+                title: "Training Tracker",
+                link: "https://github.com/wynro/airQ-web",
+                secondaryTitle: "Web Application",
+                image: "ruben.jpg",
+                description: "Proyecto para el International Space Apps Challenge 2016 de la NASA."
+            }
+        ];
+
     }]);
